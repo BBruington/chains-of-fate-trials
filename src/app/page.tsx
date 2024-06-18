d4c8461f84183aa3755ef2df466933c17fb24edb
BIBruington <psychological_chemist@hotmail.com>
2024-06-18 15:48:46 -0500

    examples for reflect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,57 @@
 "use client";
+import { DndContext, DragEndEvent } from "@dnd-kit/core";
+import Droppable from "@/components/dndkit/dropable";
+import Draggable from "@/components/dndkit/draggable";
+import { Reflect } from "@rocicorp/reflect/client";
+import { mutators } from "./utils/reflect/mutators";
+import { useSubscribe } from "@rocicorp/reflect/react";
 
 export default function Home() {
+  const reflect = new Reflect({
+    server: "http://127.0.0.1:8080/",
+    roomID: "myRoom",
+    userID: "myUser",
+    mutators,
+  });
+
+  const count = useSubscribe(reflect, (tx) => tx.get<number>("count"), 0);
+  const dragEvent = useSubscribe(
+    reflect,
+    (tx) => tx.get<string | null>("dragevent"),
+    null
+  );
+
+  const handleDragEnd = async (event: DragEndEvent) => {
+    const { over } = event;
+    const newId = over ? over.id : null;
+    await reflect.mutate.handleDragEnd(typeof newId === "string" ? newId : null);
+  };
+
+  const handleCount = async () => {
+    await reflect.mutate.increment(1);
+  };
+
+  const containers = ["A", "B", "C"];
+  const draggableMarkup = (
+    <Draggable className="bg-primary text-secondary" id="draggable">
+      Drag me
+    </Draggable>
+  );
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button className="bg-secondary h-12 w-32" onClick={handleCount}>
+        {count}
+      </button>
+      <DndContext onDragEnd={handleDragEnd}>
+        {dragEvent === null ? draggableMarkup : null}
+
+        {containers.map((id) => (
+          <Droppable key={id} id={id}>
+            {dragEvent === id ? draggableMarkup : "Drop here"}
+          </Droppable>
+        ))}
+      </DndContext>
     </main>
   );
 }

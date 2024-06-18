ff7d61b404df5a0426260a44e84f81ea3e645c3c
BIBruington <psychological_chemist@hotmail.com>
2024-06-18 15:48:46 -0500

    examples for reflect

---
+++ reflect-server/index.ts
@@ -0,0 +1,7 @@
+import { mutators } from "@/app/utils/reflect/mutators";
+ 
+export default function makeOptions() {
+  return {
+    mutators,
+  };
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 "use client";
+import { DndContext, DragEndEvent } from "@dnd-kit/core";
+import Droppable from "@/components/dndkit/dropable";
+import Draggable from "@/components/dndkit/draggable";
+import { Reflect } from "@rocicorp/reflect/client";
+import { mutators } from "./utils/reflect/mutators";
+import { useSubscribe } from "@rocicorp/reflect/react";
 
 export default function Home() {
   return (

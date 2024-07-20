0980f4ba3056c49c458dc9226bae972721fb3aeb
BIBruington <psychological_chemist@hotmail.com>
2024-07-20 18:00:25 -0500

    side nav

--- src/app/(potioncraft)/layout.tsx
+++ src/app/(potioncraft)/layout.tsx
@@ -1,19 +1,29 @@
 import Link from "next/link";
 
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const NAV_LINKS = [
+    { href: "/potioncraft", label: "Craft" },
+    { href: "/potioncraft/journal/potions", label: "Potions" },
+    { href: "/potioncraft/journal/ingredients", label: "Ingredients" },
+    { href: "/potioncraft/journal/ingredients", label: "Formulas" },
+  ];
 
   return (
     <div className="flex">
-      <div className="flex h-screen w-80 flex-col items-center space-y-3 overflow-y-auto bg-secondary p-3">
-        <Link href={"/potioncraft"}>Craft Potions</Link>
-        <Link href={"/potioncraft/journal/potions"}>Potions</Link>
-        <Link href={"/potioncraft/journal/ingredients"}>Ingredients</Link>
-        <Link href={"/potioncraft/formulas"}>Formulas</Link>
+      <div className="flex h-screen w-80 flex-col items-center space-y-2 overflow-y-auto bg-secondary/60 p-3">
+        {NAV_LINKS.map((link) => (
+          <Link
+            className="flex items-center h-10 w-full rounded-lg border-b hover:bg-secondary hover:text-primary/80"
+            key={link.label}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
       </div>
 
       {children}

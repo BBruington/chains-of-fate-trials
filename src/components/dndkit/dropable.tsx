3232a5ab183911b8a3d7e3ce2ccd5e8609423526
BIBruington <psychological_chemist@hotmail.com>
2024-07-20 16:31:19 -0500

    droppable icon

--- src/components/dndkit/dropable.tsx
+++ src/components/dndkit/dropable.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { UniqueIdentifier, useDroppable } from "@dnd-kit/core";
 import { cn } from "@/lib/utils";
 import { Ingredient } from "@prisma/client";
+import { IngredientIconProps } from "@/app/(potioncraft)/potioncraft/_types";
+import skull from "@/../public/icons/skull.svg";
+import wizardHat from "@/../public/icons/wizard-hat.svg";
+import scroll from "@/../public/icons/scroll.svg";
+import Image from "next/image";
 
 type DroppableProps = {
   id: UniqueIdentifier;
@@ -22,15 +27,29 @@ export default function Droppable({
     data: { accepts },
   });
 
+  const ingredientIcon: IngredientIconProps = {
+    ARCANE: wizardHat,
+    DIVINE: wizardHat,
+    OCCULT: skull,
+    PRIMAL: scroll,
+  };
+
   return (
     <div
       ref={setNodeRef}
       className={cn(
-        `inline-flex items-center border justify-center whitespace-nowrap rounded-md text-sm font-medium`,
+        `flex flex-col items-center justify-center space-y-5 whitespace-nowrap rounded-md border text-sm font-medium`,
         className,
       )}
     >
-      {item.name}
+      {item.id !== "empty" && (
+        <Image
+          className="w-4"
+          src={ingredientIcon[item.type as keyof IngredientIconProps]}
+          alt="Ingredient Type Icon"
+        />
+      )}
+      <div>{item.name}</div>
     </div>
   );
 }

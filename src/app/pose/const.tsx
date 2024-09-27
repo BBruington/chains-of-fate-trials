2e4b4fd641becd9e32f97e5e9f0d1abdce2575a1
hhhssh <hanshermawan1@gmail.com>
2024-09-26 17:28:36 -0700

    Adding new file

---
+++ src/app/pose/const.tsx
@@ -0,0 +1,16 @@
+export { initalImageArray };
+
+const initalImageArray = [
+  { letter: "M", image: "/Pose/Pose1.JPG", active: false, completed: false },
+  { letter: "N", image: "/Pose/Pose2.JPG", active: false, completed: false },
+  { letter: "O", image: "/Pose/Pose3.JPG", active: false, completed: false },
+  { letter: "P", image: "/Pose/Pose4.JPG", active: false, completed: false },
+  { letter: "Q", image: "/Pose/Pose5.JPG", active: false, completed: false },
+  { letter: "R", image: "/Pose/Pose6.JPG", active: false, completed: false },
+  { letter: "S", image: "/Pose/Pose7.JPG", active: false, completed: false },
+  { letter: "T", image: "/Pose/Pose8.JPG", active: false, completed: false },
+  { letter: "U", image: "/Pose/Pose9.JPG", active: false, completed: false },
+  { letter: "V", image: "/Pose/Pose10.JPG", active: false, completed: false },
+  { letter: "W", image: "/Pose/Pose11.JPG", active: false, completed: false },
+  { letter: "X", image: "/Pose/Pose12.JPG", active: false, completed: false },
+];

4e4cb6b02009f51094f8a43b20494177a7f75181
hhhssh <hanshermawan1@gmail.com>
2024-10-14 14:48:17 -0700

    Update

--- src/app/pose/actions.ts
+++ src/app/pose/actions.ts
@@ -1,35 +1,35 @@
-"use server";
-import { z } from "zod";
-import { CoordinatesSelectSchema } from "../../../prisma/generated/zod";
-import { prisma } from "../utils/context";
+// "use server";
+// import { z } from "zod";
+// import { CoordinatesSelectSchema } from "../../../prisma/generated/zod";
+// import { prisma } from "../utils/context";
 
-const ChangeHeadCoordinatesSchema = z.object({
-  coordinates: CoordinatesSelectSchema.optional(),
-  head: z.object({
-    x: z.number(),
-    y: z.number(),
-  }),
-});
+// const ChangeHeadCoordinatesSchema = z.object({
+//   coordinates: CoordinatesSelectSchema.optional(),
+//   head: z.object({
+//     x: z.number(),
+//     y: z.number(),
+//   }),
+// });
 
-export const changeHeadCoordiantes = async (props) => {
-  try {
-    const { head } = ChangeHeadCoordinatesSchema.parse(props);
-    console.log(head);
+// export const changeHeadCoordiantes = async (props) => {
+//   try {
+//     const { head } = ChangeHeadCoordinatesSchema.parse(props);
+//     console.log(head);
 
-    const headCoordinatesChange = await prisma.coordinates.update({
-      where: {
-        id: "1",
-      },
-      data: {
-        head: {
-          x: head.x,
-          y: head.y,
-        },
-      },
-    });
+//     const headCoordinatesChange = await prisma.coordinates.update({
+//       where: {
+//         id: "1",
+//       },
+//       data: {
+//         head: {
+//           x: head.x,
+//           y: head.y,
+//         },
+//       },
+//     });
 
-    return headCoordinatesChange;
-  } catch (error) {
-    console.error("Failed to change Head Coordinates", error);
-  }
-};
+//     return headCoordinatesChange;
+//   } catch (error) {
+//     console.error("Failed to change Head Coordinates", error);
+//   }
+// };
